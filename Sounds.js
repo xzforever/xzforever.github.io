6b05d21448b94161967f116e31b8297a70695e1a
xzforever <65832359+xzforever@users.noreply.github.com>
2020-11-26 23:11:40 +0300

    Add files via upload

---
+++ Sounds.js
@@ -0,0 +1,20 @@
+function passExam(){
+	let audio = new Audio();
+	audio.src="passTheExam.mp3";
+	audio.autoplay = true;
+}
+function parents(){
+	let audio = new Audio();
+	audio.src="parentsDontBelieve.mp3";
+	audio.autoplay = true;
+}
+function borned(){
+	let audio = new Audio();
+	audio.src="iWasBorned.mp3";
+	audio.autoplay = true;
+}
+function hello(){
+	let audio = new Audio();
+	audio.src="hi.mp3";
+	audio.autoplay = true;
+}
